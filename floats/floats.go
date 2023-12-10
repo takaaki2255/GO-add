4b1ef1b16bdea87bddd7e9d0af1ffe60d5802160
takaaki2255 <red.ducati.mac2255@gmail.com>
2023-12-10 17:54:32 +0900

    second

---
+++ floats/floats.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5.1
+	b := 7.2
+	// fmt.Println(3 * a)
+	// fmt.Println(3.0 * a)
+	// fmt.Println(3.3 * a)
+	fmt.Println(3 * b)
+	fmt.Println(b -a)
+	fmt.Println(6 -a)
+}
