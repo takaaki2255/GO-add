5a12af5069d09bd0c4f79016f3f4a1cc592c7b30
takaaki2255 <red.ducati.mac2255@gmail.com>
2024-01-02 22:48:20 +0900

    スライスの学

---
+++ chapter_2/intarray/intarray.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// intvals := [5]int{98, 125, 232, 147, 486}
+	// fmt.Printf("1番目の要素は%d\n", intvals[1])
+	// fmt.Printf("0番目の要素は%d\n", intvals[0])
+	// fmt.Printf("4番目の要素は%d\n", intvals[4])
+	// fmt.Printf("2番目と3３番目の要素を足すと%d\n", intvals[2]+intvals[3])
+	// intvals[1] -= 100
+	// fmt.Printf("今や1番目の要素は%d\n", intvals[1])
+
+	intvals3 := []int{98, 125, 232, 147, 487}
+	intvals3[1] -= 100
+
+	fmt.Printf("このスライスが見ている配列の要素数は%d\n", len(intvals3))
+	fmt.Printf("最後の要素数は%d\n", intvals3[len(intvals3)-1])
+	fmt.Printf("今や1番目の要素は%d\n", intvals3[1])
+}
